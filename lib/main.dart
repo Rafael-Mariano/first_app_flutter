4cc0e5ca329d4ed5731592333edf1c2ee5fcb358
Rafael Mariano <rafaelmariano312@gmail.com>
2026-07-25 16:38:06 -0300

    Adding a drawer

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,14 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter App'), centerTitle: true),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(child: Text("Drawer")),
+              ListTile(title: Text("Logout")),
+            ],
+          ),
+        ),
         floatingActionButton: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
