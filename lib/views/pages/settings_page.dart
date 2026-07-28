8a007fcf7bb3156cb1ad37474330cd962d6b1d1b
Rafael Mariano <rafaelmariano312@gmail.com>
2026-07-28 10:06:15 -0300

    Learning create alert Dialog example

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -46,6 +46,28 @@ class _SettingsPageState extends State<SettingsPage> {
                 },
                 child: Text('Open SnackBar!'),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Alert Title'),
+                        content: Text('Alert Content'),
+                        actions: [
+                          FilledButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Open Dialog!'),
+              ),
               DropdownButton(
                 value: menuItem,
                 items: [
